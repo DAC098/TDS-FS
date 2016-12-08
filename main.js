f1bd5abe7a018457ad65612230f3d9eec54178e5
DAC098 <dac098Git@gmail.com>
2016-12-08 10:13:11 -0800

    process args from command
    will now look for process args on startup and set the appropriate process and settings values if found

--- main.js
+++ main.js
@@ -3,6 +3,19 @@ global.reqRoot = function(path) {
 	return require(root + '/' + path);
 };
 
+process.env.NODE_ENV = 'development';
+
+let len = process.argv.length;
+let env_reg = /\-env/g;
+for(let c = 0; c < len; ++c) {
+	let env_res = env_reg.exec(process.argv[c]);
+	if(env_res && process.argv[c + 1] == 'production') {
+		process.env.NODE_ENV = 'production';
+	}
+}
+
+console.log('env:',process.env.NODE_ENV);
+
 global.settings = require('./lib/SettingsManager/index.js')('./settings.json');
 
 global.logger = require('./server/logging.js');
